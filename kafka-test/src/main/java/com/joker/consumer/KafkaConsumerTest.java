7900204da85be92418732bb7571cb58c5e365fc6
Joker-5 <2725284321@qq.com>
2021-11-18 14:27:32 +0800

    feat：add KafkaConsumer short demo

---
+++ kafka-test/src/main/java/com/joker/consumer/KafkaConsumerTest.java
@@ -0,0 +1,58 @@
+package com.joker.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.*;
+
+public class KafkaConsumerTest {
+    // 自动提交消费进度
+    public static void testConsumer1() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "J_ADS_ORDERCONSUMER_01");
+        props.put("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("TOPIC_ORDER"));
+        for (; ; ) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println("consumering...");
+                System.out.println("offset = " + record.offset() + ", key = " + record.key() + ", value = " + record.value());
+            }
+
+        }
+    }
+
+    //手动提交消费进度
+    public static void testConsumer2() {
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "false");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("foo", "bar"));
+        final int minBatchSize = 200;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                buffer.add(record);
+            }
+            if (buffer.size() >= minBatchSize) {
+                // insertIntoDb(buffer);
+                // 省略处理逻辑
+                consumer.commitSync();
+                buffer.clear();
+            }
+        }
+    }
+}
